479ae26bac55294a445f61796c819c1d656b8ad0
Nitya <51813914+n-cyber@users.noreply.github.com>
2024-03-15 21:45:39 -0700

    Create 1148. Article Views I.py

---
+++ 1148. Article Views I.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+    df = views[views['author_id'] == views['viewer_id']][['author_id']].rename(columns={'author_id':'id'})
+    df = df.drop_duplicates()
+    return df.sort_values(by=['id'])
